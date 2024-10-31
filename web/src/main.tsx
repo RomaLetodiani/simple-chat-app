1e2d8929008a480bb00ae1611de8e23d1bf141fe
RomaLetodiani <Roman.Letodiani03@Gmail.com>
2024-10-31 09:13:21 +0400

    ViteReactSSG

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
-import { createRoot } from "react-dom/client";
+// src/main.tsx
+import { ViteReactSSG } from "vite-react-ssg/single-page";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+export const createRoot = ViteReactSSG(<App />);
